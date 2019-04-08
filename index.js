04545e8c19f810ed1968fafcd07004d49675b48e
Julian Perez <juuprz@gmail.com>
2019-04-08 16:23:00 -0700

    Add index.html to serve local services

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const app = express();
 const port = process.env.PORT || 9001;
 
 app.use(morgan('dev'));
-app.use(express.static(path.joing(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => (console.log('ayo this yo proxy. we good over here')));
