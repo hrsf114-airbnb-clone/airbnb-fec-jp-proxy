ffa015002ef32b1d5973796b2fe543cbe20173ff
Julian Perez <juuprz@gmail.com>
2019-04-08 15:23:51 -0700

    Initialize git repo for proxy server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 9001;
+
+app.use(morgan('dev'));
+app.use(express.static(path.joing(__dirname, 'public')));
+
+app.listen(port, () => (console.log('ayo this yo proxy. we good over here')));
