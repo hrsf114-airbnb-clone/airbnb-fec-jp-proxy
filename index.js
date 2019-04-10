b1e34d574d1e850730ba97770bf9e2b0011e4c25
Julian Perez <juuprz@gmail.com>
2019-04-10 15:59:03 -0700

    Add routes within proxy server and proper component mounting & styling within index.html

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
-
+const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 9001;
 
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/rooms/:id/', express.static(path.join(__dirname, 'public')));
 
-app.listen(port, () => (console.log('ayo this yo proxy. we good over here')));
+app.get('/api/bookings/:id', (req, res) => {
+  const { id } = req.params;
+  console.log('getting julians bundle');
+  const url = `http://localhost:9000/api/bookings/${id}`;
+  axios.get(url)
+    .then(response => res.send(response.data))
+    .catch(err => res.end());
+});
+app.get('/api/listing/:id', (req, res) => {
+  const { id } = req.params;
+  console.log('getting victorias bundle');
+  const url = `http://localhost:3000/api/listing/${id}`;
+  axios.get(url)
+    .then(response => res.send(response.data))
+    .catch(err => res.end());
+});
+app.get('/api/photos/:id', (req, res) => {
+  const { id } = req.params;
+  console.log('getting evans bundle');
+  const url = `http://localhost:3001/api/photos/${id}`;
+  axios.get(url)
+    .then(response => res.send(response.data))
+    .catch(err => res.end());
+});
+
+app.listen(port, () => (console.log('ayo this yo proxy. we good over here on port ', port)));
